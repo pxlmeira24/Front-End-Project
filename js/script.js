80bf7b6eedcd0302e7593213a490fadb79b58f5d
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-20 11:32:28 -0300

    att final

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-// pegar o elemento e clonar 
 const cloneSlide = document.querySelector('.logos-slide').cloneNode(true);
 
-// colocar o elemento clonado dentro do container 
 document.querySelector('.logos').appendChild(cloneSlide);
 
 
